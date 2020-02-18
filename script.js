d51ca984d162af46f77052583afb86ee9492b4bd
David Ghazi <davidaghazi@gmail.com>
2020-02-17 19:27:09 -0500

    fixed timer

--- script.js
+++ script.js
@@ -1,44 +1,17 @@
-var startquiz = document.querySelector("#startquiz")
-var timer = document.querySelector("#timer")
+var startquiz = document.querySelector("#startquiz");
+var timer = document.querySelector("#timer");
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var count = 10;
 
-
-function startTimer() {
-    // setTime();
-    /* the "interval" variable here using "setInterval()" begins the recurring increment of the 
-       secondsElapsed variable which is used to check if the time is up */
-    interval = setInterval(function () {
-        secondsElapsed++;
-        //So renderTime() is called here once every second.
-        renderTime();
-    }, 1000);
-}
-
-// function setTime() {
-//     var minutes;
-
-//     if (status === "Working") {
-//         minutes = workMinutesInput.value.trim();
-//     } else {
-//         minutes = restMinutesInput.value.trim();
-//     }
-
-//     clearInterval(interval);
-//     totalSeconds = minutes * 60;
-// }
-
-startquiz.addEventListener("click", startTimer)
-
-var counter;
-
-function countClicks(){
-
-    if( !counter ){//counter is not set
-
-        counter = setInterval(timer, 1000);
-
-    } else {
-
-        document.getElementById('click').value = ++clicks;
-        document.getElementById('result').innerHTML = clicks;
+var quizTimer;
+startquiz.addEventListener("click", function clock(){
+    quizTimer = setInterval(myClock, 1000);
+    function myClock(){
+        timer.innerHTML = count--;
+        if(count === 0){
+            clearInterval(quizTimer);
+        }
     }
-}
+})
