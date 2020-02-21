dd97a99e40e2e510b82ebd4d052e2c29d36354f1
David Ghazi <davidaghazi@gmail.com>
2020-02-21 14:33:30 -0500

    new javascript code

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 var startTimer = document.querySelector("#startquiz");
-var startQuiz = document.querySelector("#starquiz");
 var timer = document.querySelector("#timer");
 var instructions = document.querySelector('.instructions')
 var questionContainer = document.querySelector('#question-container')
-var nextButton = document.querySelector("#next-btn")
-var finishButton = document.querySelector("#finish-btn")
-let shuffledQuestions, currentQuestionsIndex
 
-var questionElement = document.querySelector('#question')
-var answerButtonsELement = document.querySelector('#answer-buttons')
+var quizQuestion = document.querySelector('#firstQuestion')
+var option1 = document.querySelector('#option1')
+var option2 = document.querySelector('#option2')
+var option3 = document.querySelector('#option3')
+var option4 = document.querySelector('#option4')
 
-var count = 75;
+index = 0;
 
-nextButton.addEventListener('click', () =>{
-    currentQuestionsIndex ++
-    setNextQuestion()
-})
-
-var quizTimer;
 startTimer.addEventListener("click", function clock(){
-    quizTimer = setInterval(myClock, 1000);
+    instructions.classList.add('hide')
+    questionContainer.classList.remove('hide')
+    var quizTimer = setInterval(myClock, 1000);
+    var count = 75;
     function myClock(){
         timer.innerHTML = count--;
         if(count === 0){
@@ -28,107 +24,42 @@ startTimer.addEventListener("click", function clock(){
     }
 })
 
-startTimer.addEventListener("click", function startQuiz(){
-    instructions.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionsIndex = 0
-    questionContainer.classList.remove('hide')
-    setNextQuestion()
-})
+var app = {
+        questions:[
+            {q1:'Arrays in Javascript can be used to store ________.', 
+            options1:['Numbers and Strings', 'Other Arrays', 'Booleans', 
+                'All of the above'],
+            answer:1},
 
-function setNextQuestion(){
-    resetState()
-    showQuestion (shuffledQuestions[currentQuestionsIndex])
+            {q2:'String values must be enclosed within ________ when being assigned to variables.', 
+            options2:['Commas', 'Curly Brackets', 'Quotes', 
+                'Parantheses'],
+            answer:1},
+
+            {q3:'Commonly used data types DO NOT include:', 
+            options3:['Strings', 'Booleans', 'Alerts', 
+                'Numbers'],
+            answer:1},
+
+            {q4:'The condition in an if / else statement is enclosed within ________.', 
+            options4:['Quotes', 'Curly Brackets', 'Parantheses', 
+                'Square Brackets'],
+            answer:1},
+
+            {q5:'A very useful tool used during development and debugging for printing content to the debugger is:', 
+            options5:['Javascript', 'Terminal/Bash', 'For Loops', 
+                'Console Log'],
+            answer:1},
+        ],
+        
 }
 
-function showQuestion(question){
-    questionElement.innerText = question.question
-    question.answer.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct){
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsELement.appendChild(button)
-    })
+function test(){
+    quizQuestion.innerHTML=app.questions[index].q1;
+    option1.innerHTML=app.questions[index].options1[0]
+    option2.innerHTML=app.questions[index].options1[1]
+    option3.innerHTML=app.questions[index].options1[2]
+    option4.innerHTML=app.questions[index].options1[3]
 }
 
-function resetState(){
-    nextButton.classList.add('hide')
-    while (answerButtonsELement.firstChild){
-        answerButtonsELement.removeChild
-        (answerButtonsELement.firstChild)
-    }
-}
-
-function selectAnswer (e){
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsELement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionsIndex + 1){
-        nextButton.classList.remove('hide')
-    }else {
-        finishButton.classList.remove('hide')
-    }
-}
-
-function setStatusClass(element, correct){
-    if (correct){
-        element.classList.add('correct')
-    }else {
-        element.classList.add('wrong')
-    }
-}
-
-var questions = [
-    {
-        question: 'Arrays in Javascript can be used to store ________.',
-        answer: [
-            {text: 'Numbers and Strings', correct: true},
-            {text: 'Other Arrays', correct: false},
-            {text: 'Booleans', correct: false},
-            {text: 'All of the Above', correct: false}
-        ]
-    },
-    {
-        question: 'String values must be enclosed within ________ when being assigned to variables. ',
-        answer: [
-            {text: 'Commas', correct: true},
-            {text: 'Curly Brackets', correct: false},
-            {text: 'Quotes', correct: false},
-            {text: 'Parentheses', correct: false}
-        ]
-    },
-    {
-        question: 'Commonly used data types DO NOT include:',
-        answer: [
-            {text: 'Strings', correct: true},
-            {text: 'Booleans', correct: false},
-            {text: 'Alerts', correct: false},
-            {text: 'Numbers', correct: false}
-        ]
-    },
-    {
-        question: 'The condition in an if / else statement is enclosed within ________.',
-        answer: [
-            {text: 'Quotes', correct: true},
-            {text: 'Curly Brackets', correct: false},
-            {text: 'Parantheses', correct: false},
-            {text: 'Square Brackets', correct: false}
-        ]
-    },
-    {
-        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-        answer: [
-            {text: 'Javascript', correct: true},
-            {text: 'Terminal/Bash', correct: false},
-            {text: 'For Loops', correct: false},
-            {text: 'Console Log', correct: false}
-        ]
-    },
-]
+test()
