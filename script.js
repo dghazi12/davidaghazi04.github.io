727533f10aed3b14de6fbf3424aef5e7953ac2f7
David Ghazi <davidaghazi@gmail.com>
2020-02-27 01:23:32 -0500

    fixed local storage, now stores mutiple scores in the highscore page

--- script.js
+++ script.js
@@ -1,161 +1,173 @@
 var startQuiz = document.querySelector("#start-btn");
-var timerNumber = document.querySelector("#timer-number")
-var instructions = document.querySelector('.instructions')
-var questionContainer = document.querySelector('#question-container')
-let currentQuestionsIndex
+var timerNumber = document.querySelector("#timer-number");
+var instructions = document.querySelector('.instructions');
+var questionContainer = document.querySelector('#question-container');
+let currentQuestionsIndex;
 
-var questionElement = document.querySelector('#question')
-var answerButtonsElement = document.querySelector('#answer-buttons')
-var nextButton = document.querySelector('#next-btn')
-var finishButton = document.querySelector('#finish-btn')
-var userScore = document.querySelector('.user-score')
-var scoreNumber = document.querySelector('#score-number')
-var submitButton = document.querySelector('#button-addon2')
-var highscore = document.querySelector('.highscore')
-var backButton = document.querySelector('#back-btn')
-var viewHighscore = document.querySelector('#view-highscore')
-var clearHighscore = document.querySelector('#clear-btn')
+var questionElement = document.querySelector('#question');
+var answerButtonsElement = document.querySelector('#answer-buttons');
+var nextButton = document.querySelector('#next-btn');
+var finishButton = document.querySelector('#finish-btn');
+var userScore = document.querySelector('.user-score');
+var scoreNumber = document.querySelector('#score-number');
+var highscore = document.querySelector('.highscore');
+var backButton = document.querySelector('#back-btn');
+var viewHighscore = document.querySelector('#view-highscore');
+var clearHighscore = document.querySelector('#clear-btn');
+var smallContainer = document.querySelector('.small-container');
 
 var myTimer;
 var count = 75;
 
 var myClock = function (myClock) {
-    count--
+    count--;
     timerNumber.innerHTML = count;
     if (count === 0) {
         clearInterval(myTimer);
-    }
-}
+    };
+};
 
 function clock() {
-    instructions.classList.add('hide')
-    questionContainer.classList.remove('hide')
+    instructions.classList.add('hide');
+    questionContainer.classList.remove('hide');
     myTimer = setInterval(myClock, 1000);
-}
+};
 
 startQuiz.addEventListener("click", function () {
-    startGame()
-    clock()
-})
+    startGame();
+    clock();
+});
 
 nextButton.addEventListener('click', function () {
-    currentQuestionsIndex++
-    setNextQuestion()
-})
+    currentQuestionsIndex++;
+    setNextQuestion();
+});
 
 function startGame() {
-    questionContainer.classList.remove('hide')
-    startQuiz.classList.add('hide')
-    currentQuestionsIndex = 0
-    setNextQuestion()
-}
+    questionContainer.classList.remove('hide');
+    startQuiz.classList.add('hide');
+    currentQuestionsIndex = 0;
+    setNextQuestion();
+};
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(questions[currentQuestionsIndex])
-}
+    resetState();
+    showQuestion(questions[currentQuestionsIndex]);
+};
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question;
     question.answer.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
+        var button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
         if (answer.correct) {
             button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
+        };
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+};
 
 function resetState() {
-    nextButton.classList.add('hide')
+    nextButton.classList.add('hide');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
+    };
+};
 
 function selectAnswer(e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
+    var selectedButton = e.target;
+    var correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
     Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
+        setStatusClass(button, button.dataset.correct);
+    });
     if (questions.length > currentQuestionsIndex + 1) {
-        nextButton.classList.remove('hide')
+        nextButton.classList.remove('hide');
     } else {
-        finishButton.classList.remove('hide')
-    }
-}
+        finishButton.classList.remove('hide');
+    };
+};
 
 function setStatusClass(e, correct) {
-    clearStatusClass(e)
+    clearStatusClass(e);
     if (correct) {
-        e.classList.add('correct')
+        e.classList.add('correct');
     } else {
-        e.classList.add('wrong')
+        e.classList.add('wrong');
         e.addEventListener('click', function () {
-            count = count - 10
-        })
-    }
-}
+            count = count - 10;
+        });
+    };
+};
 
 function clearStatusClass(e) {
-    e.classList.remove('correct')
-    e.classList.remove('wrong')
-}
+    e.classList.remove('correct');
+    e.classList.remove('wrong');
+};
 
 finishButton.addEventListener('click', function () {
-    clearInterval(myTimer)
-    userScore.classList.remove('hide')
-    questionContainer.classList.add('hide')
-    finishButton.classList.add('hide')
-    scoreNumber.innerText = count
-})
+    clearInterval(myTimer);
+    userScore.classList.remove('hide');
+    questionContainer.classList.add('hide');
+    finishButton.classList.add('hide');
+    scoreNumber.innerText = count;
+});
 
-viewHighscore.addEventListener('click', function () {
-    clearInterval(myTimer)
-    userScore.classList.add('hide')
-    questionContainer.classList.add('hide')
-    finishButton.classList.add('hide')
-    startQuiz.classList.add('hide')
-    instructions.classList.add('hide')
-    highscore.classList.remove('hide')
-})
-
-submitButton.addEventListener('click', function () {
-    highscore.classList.remove('hide')
-    userScore.classList.add('hide')
-})
-        
 backButton.addEventListener('click', function () {
-    instructions.classList.remove('hide')
-    highscore.classList.add('hide')
-    startQuiz.classList.remove('hide')
+    instructions.classList.remove('hide');
+    startQuiz.classList.remove('hide');
     window.location.reload();
-})
+});
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('item');
+
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+const liMaker = text => {
+    const li = document.createElement('li');
+    li.textContent = text + count;
+    ul.appendChild(li);
+};
+
+form.addEventListener('submit', function (e) {
+    smallContainer.classList.remove('hide');
+    e.preventDefault();
+
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    liMaker(input.value);
+    input.value = '';
+});
+
+data.forEach(item => {
+    liMaker(item);
+});
 
 clearHighscore.addEventListener('click', function () {
-    localStorage.clear(initials)
-    lsOutput.classList.add('hide')
-    console.log(localStorage)
-})
+    localStorage.clear();
+    lsOutput.classList.add('hide');
+});
 
-var enterInitials = document.querySelector('.form-control')
-var submitToStorage = document.querySelector('#button-addon2')
-var lsOutput = document.querySelector('#lsOutput');
-
-submitToStorage.addEventListener('click', function () {
-    var initials = enterInitials.value + ': ' + count
-    let initialsString = JSON.stringify(initials);
-
-    lsOutput.innerHTML += `${initials} <br />`;
-    localStorage.getItem(initials);
-    localStorage.setItem("Highscore", initialsString);
-    console.log(localStorage);
-})
+viewHighscore.addEventListener('click', function () {
+    clearInterval(myTimer);
+    lsOutput.classList.remove('hide');
+    console.log('clicked');
+    localStorage.getItem(li);
+    // userScore.classList.add('hide');
+    // questionContainer.classList.add('hide');
+    // finishButton.classList.add('hide');
+    // startQuiz.classList.add('hide');
+    // instructions.classList.add('hide');
+    // highscore.classList.remove('hide');
+});
 
 var questions = [
     {
@@ -204,243 +216,4 @@ var questions = [
             { text: 'Console Log', correct: true }
         ]
     },
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var startQuiz = document.querySelector("#start-btn");
-// var timerNumber = document.querySelector("#timer-number")
-// var instructions = document.querySelector('.instructions')
-// var questionContainer = document.querySelector('#question-container')
-// let currentQuestionsIndex
-
-// var questionElement = document.querySelector('#question')
-// var answerButtonsElement = document.querySelector('#answer-buttons')
-// var nextButton = document.querySelector('#next-btn')
-// var finishButton = document.querySelector('#finish-btn')
-// var userScore = document.querySelector('.user-score')
-// var scoreNumber = document.querySelector('#score-number')
-// var submitButton = document.querySelector('#button-addon2')
-// var highscore = document.querySelector('.highscore')
-// var backButton = document.querySelector('#back-btn')
-// var viewHighscore = document.querySelector('#view-highscore')
-// var clearHighscore = document.querySelector('#clear-btn')
-
-// var count = 75;
-// var myTimer;
-
-// var myClock = function (myClock) {
-//     count--
-//     timerNumber.innerHTML = count;
-//     if (count === 0) {
-//         clearInterval(myTimer);
-//     }
-// }
-
-// function clock() {
-//     instructions.classList.add('hide')
-//     questionContainer.classList.remove('hide')
-//     myTimer = setInterval(myClock, 1000);
-// }
-
-// startQuiz.addEventListener("click", function () {
-//     startGame()
-//     clock()
-// })
-
-// nextButton.addEventListener('click', function () {
-//     currentQuestionsIndex++
-//     setNextQuestion()
-// })
-
-// function startGame() {
-//     questionContainer.classList.remove('hide')
-//     startQuiz.classList.add('hide')
-//     currentQuestionsIndex = 0
-//     setNextQuestion()
-// }
-
-// function setNextQuestion() {
-//     resetState()
-//     showQuestion(questions[currentQuestionsIndex])
-// }
-
-// function showQuestion(question) {
-//     questionElement.innerText = question.question
-//     question.answer.forEach(answer => {
-//         var button = document.createElement('button')
-//         button.innerText = answer.text
-//         button.classList.add('btn')
-//         if (answer.correct) {
-//             button.dataset.correct = answer.correct
-//         }
-//         button.addEventListener('click', selectAnswer)
-//         answerButtonsElement.appendChild(button)
-//     })
-// }
-
-// function resetState() {
-//     nextButton.classList.add('hide')
-//     while (answerButtonsElement.firstChild) {
-//         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-//     }
-// }
-
-// function selectAnswer(e) {
-//     var selectedButton = e.target
-//     var correct = selectedButton.dataset.correct
-//     setStatusClass(document.body, correct)
-//     Array.from(answerButtonsElement.children).forEach(button => {
-//         setStatusClass(button, button.dataset.correct)
-//     })
-//     if (questions.length > currentQuestionsIndex + 1) {
-//         nextButton.classList.remove('hide')
-//     } else {
-//         finishButton.classList.remove('hide')
-//     }
-// }
-
-// function setStatusClass(element, correct) {
-//     clearStatusClass(element)
-//     if (correct) {
-//         element.classList.add('correct')
-//     } else {
-//         element.classList.add('wrong')
-//         element.addEventListener('click', function () {
-//             count = count - 10
-//         })
-//     }
-// }
-
-// function clearStatusClass(element) {
-//     element.classList.remove('correct')
-//     element.classList.remove('wrong')
-// }
-
-// finishButton.addEventListener('click', function () {
-//     clearInterval(myTimer)
-//     userScore.classList.remove('hide')
-//     questionContainer.classList.add('hide')
-//     finishButton.classList.add('hide')
-//     scoreNumber.innerText = count
-// })
-
-// viewHighscore.addEventListener('click', function(){
-//     clearInterval(myTimer)
-//     userScore.classList.add('hide')
-//     questionContainer.classList.add('hide')
-//     finishButton.classList.add('hide')
-//     startQuiz.classList.add('hide')
-//     instructions.classList.add('hide')
-//     highscore.classList.remove('hide')
-// })
-
-// submitButton.addEventListener('click', function () {
-//     highscore.classList.remove('hide')
-//     userScore.classList.add('hide')
-// })
-
-// var enterInitials = document.querySelector('.form-control')
-// var submitToStorage = document.querySelector('#button-addon2')
-// var lsOutput = document.querySelector('#lsOutput');
-// var count = 75; 
-
-// submitToStorage.addEventListener('click', function () {
-//     var initials = enterInitials.value + ': ' + count
-//     lsOutput.innerHTML += `${initials} <br />`;
-//     let initialsString = JSON.stringify(initials);
-//     console.log(initialsString)
-
-//     localStorage.setItem("initials", initialsString);
-
-//     let initialsNormal = JSON.parse(localStorage.getItem("initials"));
-//     console.log(initialsString)
-// })
-
-// backButton.addEventListener('click', function(){
-//     instructions.classList.remove('hide')
-//     highscore.classList.add('hide')
-//     startQuiz.classList.remove('hide')
-//     window.location.reload();
-// })
-
-// clearHighscore.addEventListener('click', function(){
-//     localStorage.clear(initials)
-// })
-
-// var questions = [
-//     {
-//         question: 'Arrays in Javascript can be used to store ________.',
-//         answer: [
-//             { text: 'Numbers and Strings', correct: false },
-//             { text: 'Other Arrays', correct: false },
-//             { text: 'Booleans', correct: false, },
-//             { text: 'All of the Above', correct: true }
-
-//         ]
-//     },
-//     {
-//         question: 'String values must be enclosed within ________ when being assigned to variables. ',
-//         answer: [
-//             { text: 'Commas', correct: false },
-//             { text: 'Curly Brackets', correct: false },
-//             { text: 'Quotes', correct: true },
-//             { text: 'Parentheses', correct: false }
-//         ]
-//     },
-//     {
-//         question: 'Commonly used data types DO NOT include:',
-//         answer: [
-//             { text: 'Strings', correct: false },
-//             { text: 'Booleans', correct: false },
-//             { text: 'Alerts', correct: true },
-//             { text: 'Numbers', correct: false }
-//         ]
-//     },
-//     {
-//         question: 'The condition in an if / else statement is enclosed within ________.',
-//         answer: [
-//             { text: 'Quotes', correct: false },
-//             { text: 'Curly Brackets', correct: false },
-//             { text: 'Parantheses', correct: true },
-//             { text: 'Square Brackets', correct: false }
-//         ]
-//     },
-//     {
-//         question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-//         answer: [
-//             { text: 'Javascript', correct: false },
-//             { text: 'Terminal/Bash', correct: false },
-//             { text: 'For Loops', correct: false },
-//             { text: 'Console Log', correct: true }
-//         ]
-//     },
-// ]
-
-// Check to see if there is anything being stored in the local storage.
-
-// let myObj = {
-//     name: "Domenic",
-//     age: 56
-// };
-
-// let myObj_serialized = JSON.stringify(myObj);
-// console.log(myObj_serialized)
-
-// localStorage.setItem("myObj", myObj_serialized);
-
-// let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
-// console.log(myObj_serialized)
-
-
-// console.log(localStorage)
+];
